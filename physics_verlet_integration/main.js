480867e0111a9ea9f8e2b7c882e0b7fcb2d5d7fc
Erick22 <52467300+erick22js@users.noreply.github.com>
2023-03-20 15:35:39 -0300

    Add files via upload

--- physics_verlet_integration/main.js
+++ physics_verlet_integration/main.js
@@ -11,6 +11,8 @@ function Point(x=0, y=0, vx=0, vy=0, mass=1, pinned=false){
 	
 	self.pinned = pinned;
 	
+	self.lines = [];
+	
 	self.update = function(dt){
 		if(!self.pinned){
 			var acc = g_acc.clone().rdc(self.mass);
@@ -43,14 +45,16 @@ function Point(x=0, y=0, vx=0, vy=0, mass=1, pinned=false){
 	}
 	
 	self.draw = function(){
-		drawCircle("blue", self.pos.x*g_scl, self.pos.y*g_scl, 10*g_scl);
+		drawCircle("blue", self.pos.x*g_scl, self.pos.y*g_scl, 2*g_scl);
 	}
 }
 
 function Line(p1, p2, width){
 	var self = this;
 	self.p1 = p1;
+	p1.lines.push(self);
 	self.p2 = p2;
+	p2.lines.push(self);
 	self.width = width;
 	
 	self.update = function(dt){
@@ -71,10 +75,10 @@ function Line(p1, p2, width){
 	}
 }
 
-
+/*
 const points = [
 	new Point(100, 100, 1, 0, 4),
-	new Point(200, 100, -1, 0, 4, true),
+	new Point(200, 100, -1, 0, 4),
 	new Point(200, 200, 0, -2, 4),
 	new Point(100, 200, 0, 1, 4),
 ];
@@ -86,6 +90,33 @@ const lines = [
 	new Line(points[3], points[0], 100),
 	new Line(points[0], points[2], 100*Math.sqrt(2)),
 ];
+*/
+
+const points = [
+	
+];
+
+for(var y=10; y<=250; y+=10){
+	for(var x=10; x<=490; x+=10){
+		points.push(new Point(x, y, 0, 0, 4, y==10));
+	}
+}
+
+const lines = [
+	
+];
+for(var vy=0; vy<25; vy++){
+	for(var vx=0; vx<=47; vx++){
+		lines.push(new Line(points[vx + (vy*49)], points[vx+1 + (vy*49)], 10));
+	}
+}
+
+for(var vy=0; vy<24; vy++){
+	for(var vx=0; vx<=48; vx++){
+		var line = new Line(points[vx + (vy*49)], points[vx + ((vy+1)*49)], 10);
+		lines.push(line);
+	}
+}
 
 
 App.start = function(){
@@ -98,6 +129,11 @@ App.update = function(dt){
 	/* Updates the points on screen */
 	for(var i=0; i<points.length; i++){
 		points[i].update(dt);
+		
+		if(points[i]==actual){
+			points[i].pos.x = pos[0];
+			points[i].pos.y = pos[1];
+		}
 	}
 	
 	/* Updates the lines on screen */
@@ -122,8 +158,80 @@ App.update = function(dt){
 	
 }
 
+function removePoint(p){
+	for(var pi=0; pi<points.length; pi++){
+		if(points[pi]==p){
+			points.splice(pi, 1);
+			break;
+		}
+	}
+	for(var li=0; li<lines.length; li++){
+		if(p.lines.includes(lines[li])){
+			lines.splice(li, 1);
+			li--;
+		}
+	}
+}
+
 window.onkeydown = function(ev){
 	if(ev.key=="Escape"){
 		App.running = false;
+	}
+}
+
+var actual = null;
+var pos = [0, 0];
+
+var pressed = false;
+
+window.onmousedown = function(ev){
+	var x = ev.offsetX;
+	var y = ev.offsetY;
+	pressed = true;
+	
+	for(var pi=0; pi<points.length; pi++){
+		if(points[pi].pos.distance(new Vector(x, y))<5){
+			if(ev.ctrlKey){
+				removePoint(points[pi]);
+			}
+			else{
+				actual = points[pi];
+			}
+			break;
+		}
+	}
+	
+	pos[0] = x;
+	pos[1] = y;
+	
+	console.log(ev);
+	
+}
+
+window.onmousemove = function(ev){
+	var x = ev.offsetX;
+	var y = ev.offsetY;
+	
+	for(var pi=0; pi<points.length; pi++){
+		if(points[pi].pos.distance(new Vector(x, y))<5){
+			if(ev.ctrlKey&&pressed){
+				removePoint(points[pi]);
+			}
+			break;
+		}
+	}
+	
+	pos[0] = x;
+	pos[1] = y;
+	
+}
+
+window.onmouseup = function(ev){
+	var x = ev.offsetX;
+	var y = ev.offsetY;
+	pressed = false;
+	
+	if(actual){
+		actual = null;
 	}
 }
