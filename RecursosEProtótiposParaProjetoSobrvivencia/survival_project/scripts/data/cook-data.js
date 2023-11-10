7207ec16b406b22e2a6fa5c6ef37be45ba792d18
Erick22 <52467300+erick22js@users.noreply.github.com>
2023-11-10 12:03:40 -0300

    Delete RecursosEProtótiposParaProjetoSobrvivencia directory

--- RecursosEProtótiposParaProjetoSobrvivencia/survival_project/scripts/data/cook-data.js
+++
@@ -1,8 +0,0 @@
-
-
-const COOK = [
-	{	ingredient:[{item:"clayPot", container:[["bean",30],["bean",30],["bean",30]], filled:"water"}],
-		timeForCooking: 50,
-		result:["beanGuisadeOnPot",0,1,{}]
-	}
-]
--- RecursosEProtótiposParaProjetoSobrvivencia/webgl/pixi/main.js
+++
@@ -1,4 +0,0 @@
-var cv = document.getElementById("screen");
-console.log(cv.getContext("webgl"));
-
-var core = new PIXI.Application({view:cv})
